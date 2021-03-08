8cf974a3d40c8ae18236bbc5cca45e94fc2e77ad
Dimadzu <Dimadzu@i.ua>
2021-03-08 23:57:30 +0300

    refactor: cheange function towelSort

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ module.exports = function towelSort (matrix) {
   let newArr=[];
   for(let j = 0; j < matrix.length; j++) {
      if(j%2===0){
-      for(let n = 0; n < matrix.length; n++) {
+      for(let n = 0; n < matrix[j].length; n++) {
           if(matrix[j][n]!==undefined){
           newArr.push(matrix[j][n]);}
       } 
   } else if (!j%2===0) {
-      for(let n = matrix.length-1; n>=0; n--){
+      for(let n = matrix[j].length; n>=0; n--){
           if(matrix[j][n]!==undefined){
            newArr.push(matrix[j][n]);}
        }
